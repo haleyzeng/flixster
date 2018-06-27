ae14477a36c4d7fae40842852a395ba035e9fd04
Haley Zeng <hhzeng@fb.com>
2018-06-27 11:07:49 -0700

    Initial Commit

---
+++ flixster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flixster
+//
+//  Created by Haley Zeng on 6/27/18.
+//  Copyright © 2018 Haley Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
