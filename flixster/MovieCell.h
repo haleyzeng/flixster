e1c9d7bbe0e1803542ad9f7d0adda625b762796c
Haley Zeng <hhzeng@fb.com>
2018-06-27 17:17:41 -0700

    movies listed in tableview

---
+++ flixster/MovieCell.h
@@ -0,0 +1,16 @@
+//
+//  MovieCell.h
+//  flixster
+//
+//  Created by Haley Zeng on 6/27/18.
+//  Copyright © 2018 Haley Zeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *posterImageView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *descriptionLabel;
+
+@end
--- flixster/MoviesViewController.m
+++ flixster/MoviesViewController.m
@@ -7,10 +7,14 @@
 //
 
 #import "MoviesViewController.h"
+#import "MovieCell.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface MoviesViewController ()
+@interface MoviesViewController () <UITableViewDataSource, UITableViewDelegate>
 
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 @property (nonatomic, strong) NSArray *movies;
+@property (nonatomic, strong) UIRefreshControl *refreshControl;
 
 @end
 
@@ -18,7 +22,18 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
+    
     // Do any additional setup after loading the view.
+    [self fetchMovies];
+    self.refreshControl = [[UIRefreshControl alloc] init];
+    [self.refreshControl addTarget:self action:@selector(fetchMovies) forControlEvents:UIControlEventValueChanged];
+    [self.tableView insertSubview:self.refreshControl atIndex:0];
+}
+
+- (void)fetchMovies {
     NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"];
     NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
     NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
@@ -36,13 +51,37 @@
             
             self.movies = dataDictionary[@"results"];
             for (NSDictionary *movie in self.movies) {
-                
-                
+                NSLog(@"%@", movie[@"title"]);
             }
+            
+            [self.tableView reloadData];
         }
+        [self.refreshControl endRefreshing];
     }];
     [task resume];
+
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.movies.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    MovieCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MovieCell"];
     
+    NSDictionary *movie = self.movies[indexPath.row];
+    cell.titleLabel.text = movie[@"title"];
+    cell.descriptionLabel.text = movie[@"overview"]; 
+    
+    NSString *baseURLString = @"https://image.tmdb.org/t/p/w500";
+    NSString *posterURLString = movie[@"poster_path"];
+    NSString *fullURLString = [baseURLString stringByAppendingString:posterURLString];
+    
+    NSURL *posterURL = [NSURL URLWithString:fullURLString];
+    cell.posterImageView.image = nil;
+    [cell.posterImageView setImageWithURL:posterURL];
+  //  cell.textLabel.text = movie[@"title"];
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning {
